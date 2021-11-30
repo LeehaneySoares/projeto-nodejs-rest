c61661ca2b5bac01b489b439436143c22569e44e
Leehaney Soares <leehaney.soares@gmail.com>
2021-11-30 19:49:13 -0300

    refactor(index): fixando conteúdo

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
 const app = express()
 
-app.listen(3000, console.log('Olá, você está conectado na porta 3000'))
-
-app.get('/atendimentos', (request, response) => response.send('Você está em atendimentos'))
+app.listen('3000', console.log('Servidor rodando na porta 3000'))
+app.get('/atendimentos', (request, response) => response.send('Olá, agora vocês está nos nossos atendimentos'))
