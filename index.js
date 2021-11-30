0264d959067f2c1b8124aa126bd1a21fddc831f0
Leehaney Soares <leehaney.soares@gmail.com>
2021-11-30 20:03:06 -0300

    feat(principal): instalando nodemon para facilitar na atualização do servidor

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.listen('3000', console.log('Servidor rodando na porta 3000'))
-app.get('/atendimentos', (request, response) => response.send('Olá, agora vocês está nos nossos atendimentos'))
+app.get('/atendimentos', (request, response) => response.send('Bem vindo, você está em atendimentos'))
