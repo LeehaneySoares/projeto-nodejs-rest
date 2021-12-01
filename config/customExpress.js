13a7ce504a8f1c0585338e360dc3e75674bd6810
Leehaney Soares <leehaney.soares@gmail.com>
2021-12-01 20:14:57 -0300

    feat(config): adicionando body-parser para conversão de request

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')
 
 module.exports = () => {
   const app = express()
+  app.use(bodyParser.urlencoded({ extended: true }))
   consign()
     .include('controllers')
     .into(app)
