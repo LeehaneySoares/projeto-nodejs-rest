e6aa8aa199bc6e99bfcc4655f1d23670f5a89a9d
Leehaney Soares <leehaney.soares@gmail.com>
2021-11-30 21:49:57 -0300

    feat(controllers): adicionando um POST para teste na requisição

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
-  app.get('/atendimentos', (request, response) => response.send('Vocês está em atendimentos'))
+  app.get('/atendimentos', (request, response) => response.send('Vocês está fazendo um GET'))
+
+  app.post('/atendimentos', (request, response) => response.send('Você está fazendo um POST'))
 }
